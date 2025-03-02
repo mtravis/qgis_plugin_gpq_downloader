4f7a62f28a58c50f957e493d242adddb47d93ca2
mtravis <matt.travis1@gmail.com>
2025-03-02 19:51:34 +0000

    reverted parquet being cast to JSON text so that STRUCT is preserved.

--- package.sh
+++ package.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
-
-# Define the output zip filename
-ZIP_NAME="qgis_plugin_gpq_downloader.zip"
-
 # Remove any existing zip file
-rm -f "$ZIP_NAME"
+rm gpq_downloader.zip 
 
 # Create the zip, excluding unnecessary files including this script
-zip -r "$ZIP_NAME" * -x ".git*" ".github*" "release*" "*.zip" "package.sh" 
-# Print success message
-echo "Created $ZIP_NAME"
+zip -r gpq_downloader.zip gpq_downloader/ -x "*.DS_Store" "*.gitignore" "*/.git/*"
+
